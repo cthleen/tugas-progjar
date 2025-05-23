80480fe7e63b9120382eaf8ff66f87b0afe399fc
cthleen <cathleengracia24@gmail.com>
2025-05-24 01:13:32 +0700

    add tugas 3

---
+++ tugas-3/file_interface.py
@@ -0,0 +1,56 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self,params=[]):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def get(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return None
+            fp = open(f"{filename}",'rb')
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+        
+    def upload(self, params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return dict(status='ERROR', data='Nama file tidak boleh kosong')
+            fp = open(filename, 'wb')
+            fp.write(base64.b64decode(params[1]))
+            return dict(status='OK', data='File berhasil diupload')
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return dict(status='ERROR', data='Nama file tidak boleh kosong')
+            os.remove(filename)
+            return dict(status='OK', data='File berhasil dihapus')
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+
+
+
+if __name__=='__main__':
+    f = FileInterface()
+    print(f.list())
+    print(f.get(['pokijan.jpg']))
